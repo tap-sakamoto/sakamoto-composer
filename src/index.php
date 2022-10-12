166b08461e857e6000c019a8b7ba65ae6d7367ad
omoon <omooon@gmail.com>
2022-10-12 12:01:25 +0900

    add japanese holiday library

--- src/index.php
+++ src/index.php
@@ -1,4 +1,9 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-echo \Omoon\KansaiDialect\KansaiDialect::translate('いいね');
+//echo \Omoon\KansaiDialect\KansaiDialect::translate('いいね');
+
+
+$holidayRepository = new \Japanese\Holiday\Repository();
+var_dump($holidayRepository->isHoliday('2022-10-11'));
+//print_r($holidayRepository->getHolidayDate(2022, 'スポーツの日'));
