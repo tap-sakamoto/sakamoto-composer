b86d2f74f790801dc2012cbcbcce86def9a51cef
tap-sakamoto <sakamoto@fb.tapweb.jp>
2022-10-13 18:59:21 +0900

    初期化の仕方を変更。

--- src/screenshot_mypage.php
+++ src/screenshot_mypage.php
@@ -5,31 +5,30 @@ use League\Csv\Statement;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/WebsiteScreenshot.php';
-require_once __DIR__ . '/../data/test_csv.csv';
 
 try {
     $csvPath = __DIR__ . '/../data/mypage_list.csv';
-//    $csv = Reader::createFromPath($csvPath, 'r');//読み取りモードでCSVを読み取る
-//    $csv->setHeaderOffset(0);//ヘッダーの設定
-//
-//    $records = Statement::create()->process($csv);
-//    $screenData = [];//初期化
-//    foreach ($records as $record) {
-//        //mypage(4つめのカラム)が1のところのみに抽出
-//        if ($record['is_mypage'] === 1) {
-//            echo $record['clname'] . "\n";
-//            $screenData[$record['clname'] . 'exEvent'] = 'https://mypage.s-axol.jp/' . $record['nickname'];
-//            $screenData[$record['clname'] . 'Event'] = 'https://mypage.s-axol.jp/' . $record['nickname'] . '/events';
-//        } else {
-//            //処理しない
-//        }
-//
-//    }
-//    var_dump($screenData);
-////
-//    $getScreenshot = new WebsiteScreenshot();
-//    $getScreenshot->changeScreenshotSize(null, 4000);
-//    $getScreenshot->saveScreenshot('mypageScreenshot', $screenData);
+    $csv = Reader::createFromPath($csvPath,'r');//読み取りモードでCSVを読み取る
+    $csv->setHeaderOffset(0);//ヘッダーの設定
+
+    $records = Statement::create()->process($csv);
+    $screenData = [];//初期化
+    foreach ($records as $record) {
+        //mypage(4つめのカラム)が1のところのみに抽出
+        if ($record['is_mypage'] === '1') {
+            echo $record['clname'] . "\n";
+            $screenData[$record['clname'] . 'exEvent'] = 'https://mypage.s-axol.jp/' . $record['nickname'];
+            $screenData[$record['clname'] . 'Event'] = 'https://mypage.s-axol.jp/' . $record['nickname'] . '/events';
+        } else {
+            //処理しない
+        }
+
+    }
+    var_dump($screenData);
+
+    $getScreenshot = new WebsiteScreenshot();
+    $getScreenshot->changeScreenshotSize(null, 4000);
+    $getScreenshot->saveScreenshot('mypageScreenshot', $screenData);
 
 } catch (Exception $ex) {
     print "エラー!: " . $ex->getMessage() . "\n";
